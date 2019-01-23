e9d2b8a9b9434f6401559d071c50f5369c9636b8
redeyes361 <redeyes_361@hotmail.com>
2019-01-23 10:23:57 +0100

    Create exeption and tempUtil class

---
+++ src/main/java/m2/exceptions/CopyException.java
@@ -0,0 +1,19 @@
+package m2.exceptions;
+
+/**
+ * 
+ * @author DESCHAMPS Mathieu && ESNAULT Jeremie
+ *
+ */
+public class CopyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public CopyException( String message ){
+		super(message);
+	}
+
+}
