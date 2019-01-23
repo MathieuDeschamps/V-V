b5a48ff508e093eca7ee9978c83aa167459e20a0
redeyes361 <redeyes_361@hotmail.com>
2019-01-23 11:54:34 +0100

    tutu

--- src/main/java/m2/vv/code_coverage/TraceFunctionCall.java
+++ src/main/java/m2/vv/code_coverage/TraceFunctionCall.java
@@ -1,5 +1,6 @@
 package m2.vv.code_coverage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,15 +19,37 @@ import javassist.NotFoundException;
 public class TraceFunctionCall {
 
 	public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
+		
+		if( args.length != 1 )
+		{
+			System.out.println("Error we need a maeven projrct path as parameter");
+			System.exit(-1);
+		}
+		String projectPath = args[0];
+		File maevenDirectory = new File( args[0]);
+		if( !(maevenDirectory.exists( ) && maevenDirectory.isDirectory( ) ) )
+		{
+			System.out.println("The path passed as argument is not valid");
+			System.exit(-1);
+			
+		}
+		String classTarget  = projectPath +"/target/classes";
+		File classesDirectory = new File( classTarget );
+		if( ! (classesDirectory.exists( ) && classesDirectory.isDirectory( ) ) ){
+			
+			System.out.println( "Target classes are missing");	
+			System.exit( -1 );
+		}
+		
 		JUnitCore core = new JUnitCore();
-		URL classUrl = new URL("file:///home/jeremy/Programmation/java/stack/target/classes/");
-		URL testUrl = new URL("file:///home/jeremy/Programmation/java/stack/target/test-classes/");
+		URL classUrl = new URL("file://"+ args[0]+"/target/classes/");
+		URL testUrl = new URL("file://"+ args[0]+"/target/test-classes/");
 		URL[] classUrls = { classUrl, testUrl };
 		URLClassLoader ucl = new URLClassLoader(classUrls);
 
 		ClassPool pool = ClassPool.getDefault();
 
-		final String folder = "/home/jeremy/Programmation/java/stack/target/classes/";
+		final String folder = "/home/jeremy/Programmation/java/stack/target/cl	asses/";
 		final String testFolder = "/home/jeremy/Programmation/java/stack/target/test-classes/";
 		
 		try {
@@ -57,7 +80,7 @@ public class TraceFunctionCall {
 		}	
 
 		for (CtMethod method : functions.getDeclaredMethods()) {
-
+			
 			String instruction = String.format("{System.out.println(\"%s\");}",
 					"-" + method.getName() + " ;");
 			try {
